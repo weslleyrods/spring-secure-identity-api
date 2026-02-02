a2f24c46e756af24d64e8b48160c25ed721cd2b6
Weslley Rodrigues <weslley.rdgs@gmail.com>
2026-02-02 18:40:54 -0300

    chore(user): tests partial update and change role

--- src/test/java/com/weslley/ssi_api/service/UserServiceTest.java
+++ src/test/java/com/weslley/ssi_api/service/UserServiceTest.java
@@ -1,11 +1,14 @@
 package com.weslley.ssi_api.service;
 
 import com.weslley.ssi_api.dto.user.UserCreateDTO;
+import com.weslley.ssi_api.dto.user.UserRoleDTO;
 import com.weslley.ssi_api.exception.UserAlreadyExistsException;
 import com.weslley.ssi_api.model.UserModel;
+import com.weslley.ssi_api.model.enums.UserRole;
 import com.weslley.ssi_api.repository.RefreshTokenRepository;
 import com.weslley.ssi_api.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -94,6 +97,50 @@ public class UserServiceTest {
         assertEquals(userCreateDTO.getName(), result.getName());
         assertEquals(userCreateDTO.getEmail(), result.getEmail());
         assertEquals(userCreateDTO.getPassword(), result.getPassword());
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    void partialUpdateSuccess(){
+        UserModel user = new UserModel();
+        user.setId(1L);
+        user.setName("User Test");
+        user.setEmail("user@email.com");
+        user.setPassword("123456");
+
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setName("User Test Partial Edited");
+        userCreateDTO.setEmail("user_partial_edited@email.com");
+
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        UserModel result = userService.partialUpdate(user.getId(), userCreateDTO);
+        assertEquals(userCreateDTO.getName(), result.getName());
+        assertEquals(userCreateDTO.getEmail(), result.getEmail());
+        assertEquals("123456", result.getPassword());
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    void changeRoleSuccess(){
+        UserModel user = new UserModel();
+        user.setId(1L);
+        user.setName("User Test");
+        user.setEmail("user@email.com");
+        user.setPassword("123456");
+        user.setRole(UserRole.USER);
+
+        UserRoleDTO userRoleDTO = new UserRoleDTO();
+        userRoleDTO.setRole(UserRole.ADMIN);
+        
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        UserModel result = userService.changeRole(user.getId(), userRoleDTO);
+        assertEquals(1L, result.getId());
+        assertEquals("User Test", result.getName());
+        assertEquals("user@email.com", result.getEmail());
+        assertEquals("123456", result.getPassword());
+        assertEquals(UserRole.ADMIN, result.getRole());
         Mockito.verify(userRepository, Mockito.times(1)).save(user);
     }
 
