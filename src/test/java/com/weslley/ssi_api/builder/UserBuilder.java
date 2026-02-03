be029e9b5fd09bfb9732580cacb0ba01ddfdf89b
Weslley Rodrigues <weslley.rdgs@gmail.com>
2026-02-03 17:49:41 -0300

    chore(user): creates a builder class

---
+++ src/test/java/com/weslley/ssi_api/builder/UserBuilder.java
@@ -0,0 +1,52 @@
+package com.weslley.ssi_api.builder;
+
+import com.weslley.ssi_api.model.UserModel;
+import com.weslley.ssi_api.model.enums.UserRole;
+
+public class UserBuilder {
+
+    private Long id = 1L;
+    private String name = "User Test";
+    private String email = "user@email.com";
+    private String password = "123456";
+    private UserRole role = UserRole.USER;
+    // Static method to start the builder
+    public static UserBuilder aUser() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder withRole(UserRole role) {
+        this.role = role;
+        return this;
+    }
+
+    public UserModel build() {
+        UserModel user = new UserModel();
+        user.setId(id);
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+        return user;
+    }
+}
